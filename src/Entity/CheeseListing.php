8698daff83688fe6a99f2330b1062a91301d8993
Abentaher <abentaher@greenflex.com>
2021-04-21 20:47:49 +0200

    add createdAt column

--- src/Entity/CheeseListing.php
+++ src/Entity/CheeseListing.php
@@ -39,6 +39,11 @@ class CheeseListing
      */
     private $isPublished;
 
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $createdAt;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -88,6 +93,18 @@ class CheeseListing
     public function setIsPublished(bool $isPublished): self
     {
         $this->isPublished = $isPublished;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
 
         return $this;
     }
