05d52f6d150ab4594428e82d158019be601d4204
Abentaher <abentaher@greenflex.com>
2021-04-28 17:57:04 +0200

    Automatic 404 on Unpublished Items

---
+++ src/ApiExtension/CheeseListingIsPublishedExtension.php
@@ -0,0 +1,60 @@
+<?php
+
+
+namespace App\ApiExtension;
+
+
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Extension\QueryCollectionExtensionInterface;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Extension\QueryItemExtensionInterface;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Util\QueryNameGeneratorInterface;
+use App\Entity\CheeseListing;
+use Doctrine\ORM\QueryBuilder;
+use Symfony\Component\Security\Core\Security;
+
+class CheeseListingIsPublishedExtension implements QueryCollectionExtensionInterface, QueryItemExtensionInterface
+{
+    private $security;
+
+    public function __construct(Security $security)
+    {
+
+        $this->security = $security;
+    }
+    public function applyToCollection(
+        QueryBuilder $queryBuilder,
+        QueryNameGeneratorInterface $queryNameGenerator,
+        string $resourceClass,
+        string $operationName = null
+    ) {
+        $this->addWhere($resourceClass, $queryBuilder);
+    }
+
+    public function applyToItem(
+        QueryBuilder $queryBuilder,
+        QueryNameGeneratorInterface $queryNameGenerator,
+        string $resourceClass,
+        array $identifiers,
+        string $operationName = null,
+        array $context = []
+    ) {
+        $this->addWhere($resourceClass, $queryBuilder);
+    }
+
+    /**
+     * @param string $resourceClass
+     * @param QueryBuilder $queryBuilder
+     */
+    private function addWhere(string $resourceClass, QueryBuilder $queryBuilder): void
+    {
+        if ($resourceClass !== CheeseListing::class) {
+            return;
+        }
+        if($this->security->isGranted('ROLE_ADMIN')) {
+            return;
+        }
+        $rootAlias = $queryBuilder->getRootAliases()[0];
+        $queryBuilder
+            ->andWhere(sprintf('%s.isPublished = :isPublished', $rootAlias))
+            ->setParameter('isPublished', true);
+    }
+}
--- tests/Functional/CheeseListingResourceTest.php
+++ tests/Functional/CheeseListingResourceTest.php
@@ -76,4 +76,59 @@ class CheeseListingResourceTest extends CustomApiTestCase
         ]);
         $this->assertResponseStatusCodeSame(200);
     }
+
+    public function testGetCheeseListingCollection()
+    {
+        $client = self::createClient();
+        $user = $this->createUser('cheeseplese@example.com', 'foo');
+
+        $cheeseListing1 = new CheeseListing('cheese1');
+        $cheeseListing1->setOwner($user);
+        $cheeseListing1->setPrice(1000);
+        $cheeseListing1->setDescription('cheese');
+
+        $cheeseListing2 = new CheeseListing('cheese2');
+        $cheeseListing2->setOwner($user);
+        $cheeseListing2->setPrice(1000);
+        $cheeseListing2->setDescription('cheese');
+        $cheeseListing2->setIsPublished(true);
+
+        $cheeseListing3 = new CheeseListing('cheese3');
+        $cheeseListing3->setOwner($user);
+        $cheeseListing3->setPrice(1000);
+        $cheeseListing3->setDescription('cheese');
+        $cheeseListing3->setIsPublished(true);
+
+        $em = $this->getEntityManager();
+
+        $em->persist($cheeseListing1);
+        $em->persist($cheeseListing2);
+        $em->persist($cheeseListing3);
+        $em->flush();
+
+        $client->request('GET', '/api/cheeses');
+
+        $this->assertJsonContains(['hydra:totalItems' => 2]);
+    }
+
+
+    public function testGetCheeseListingItem()
+    {
+        $client = self::createClient();
+        $user = $this->createUser('cheeseplese@example.com', 'foo');
+
+        $cheeseListing1 = new CheeseListing('cheese1');
+        $cheeseListing1->setOwner($user);
+        $cheeseListing1->setPrice(1000);
+        $cheeseListing1->setDescription('cheese');
+        $cheeseListing1->setIsPublished(false);
+
+        $em = $this->getEntityManager();
+        $em->persist($cheeseListing1);
+        $em->flush();
+
+        $client->request('GET', '/api/cheeses/'.$cheeseListing1->getId());
+
+        $this->assertResponseStatusCodeSame(404);
+    }
 }
